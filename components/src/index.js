41f3eb03e025c771046ecdf9677aa525823b6ae7
melissasedgwick <melissa.sedgwick@yahoo.co.uk>
2019-01-04 16:15:05 +0000

    Added avatar, content, and timeAgo to CommentDetail props

--- components/src/index.js
+++ components/src/index.js
@@ -6,9 +6,12 @@ import CommentDetail from './CommentDetail';
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author="Sam"/>
-      <CommentDetail author="Alex"/>
-      <CommentDetail author="Charlie"/>
+      <CommentDetail author="Sam" timeAgo="Today at 4:45PM"
+        commentText="This is a message!" avatar={Faker.image.avatar()}/>
+      <CommentDetail author="Alex" timeAgo="Today at 2:30PM"
+        commentText="Another message!" avatar={Faker.image.avatar()}/>
+      <CommentDetail author="Charlie" timeAgo="Yesterday at 3:25PM"
+        commentText="And again!" avatar={Faker.image.avatar()}/>
     </div>
   );
 };
