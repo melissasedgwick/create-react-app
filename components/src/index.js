69df71fc136591d625748d158d7e924565a3d806
melissasedgwick <melissa.sedgwick@yahoo.co.uk>
2019-01-04 15:11:15 +0000

    Added Faker to generate avatar images

--- components/src/index.js
+++ components/src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Faker from 'faker';
 
 const App = ()=> {
   return (
     <div className="ui container comments">
       <div className="comment">
-        <a href="/" className="avatar"><img alt="avatar" /></a>
+        <a href="/" className="avatar"><img alt="avatar" src={Faker.image.avatar()}/></a>
         <div className="content">
           <a href="/" className="author">Sam</a>
           <div className="metadata">
