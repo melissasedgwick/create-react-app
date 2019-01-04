e6147a9299d122499ec27432ab494108fed97002
melissasedgwick <melissa.sedgwick@yahoo.co.uk>
2019-01-04 16:40:58 +0000

    Added CommentDetail as chid to ApprovalCard

--- components/src/index.js
+++ components/src/index.js
@@ -2,16 +2,37 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Faker from 'faker';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author="Sam" timeAgo="Today at 4:45PM"
-        commentText="This is a message!" avatar={Faker.image.avatar()}/>
-      <CommentDetail author="Alex" timeAgo="Today at 2:30PM"
-        commentText="Another message!" avatar={Faker.image.avatar()}/>
-      <CommentDetail author="Charlie" timeAgo="Yesterday at 3:25PM"
-        commentText="And again!" avatar={Faker.image.avatar()}/>
+      <ApprovalCard >
+        <CommentDetail
+          author="Sam"
+          timeAgo="Today at 4:45PM"
+          commentText="This is a message!"
+          avatar={Faker.image.avatar()}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Alex"
+          timeAgo="Today at 2:30PM"
+          commentText="Another message!"
+          avatar={Faker.image.avatar()}
+        />
+      </ApprovalCard>
+      
+      <ApprovalCard>
+        <CommentDetail
+          author="Charlie"
+          timeAgo="Yesterday at 3:25PM"
+          commentText="And again!"
+          avatar={Faker.image.avatar()}
+        />
+      </ApprovalCard>
     </div>
   );
 };
