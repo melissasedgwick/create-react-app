2b6012ce76e5d4bc8762513adb2b01a01b87fda8
melissasedgwick <melissa.sedgwick@yahoo.co.uk>
2019-01-21 16:12:06 +0000

    SearchBar returns results from api request and displays number of results

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,12 +1,31 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
-const App = () => {
-  return (
-    <div className="ui container" style={{ marginTop: '10px' }}>
-      <SearchBar />
-    </div>
-  );
-};
+require('dotenv').config();
+
+class App extends React.Component {
+  state = { images: [] };
+
+  onSearchSubmit = async (term) => {
+    const response = await axios.get('https://api.unsplash.com/search/photos', {
+      params: { query: term },
+      headers: {
+        Authorization: 'Client-ID ' + process.env.REACT_APP_ACCESS_KEY
+      }
+    });
+
+    this.setState({ images: response.data.results });
+  }
+
+  render() {
+    return (
+      <div className="ui container" style={{ marginTop: '10px' }}>
+        <SearchBar onSubmit={this.onSearchSubmit}/>
+        Found: {this.state.images.length} images
+      </div>
+    );
+  }
+}
 
 export default App
