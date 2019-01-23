9300d656e9b14642f56542f333bf7e1590464ba7
melissasedgwick <melissa.sedgwick@yahoo.co.uk>
2019-01-23 15:37:35 +0000

    Formatted display of videos

--- videos/src/components/VideoDetail.js
+++ videos/src/components/VideoDetail.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const VideoDetail = ({ video }) => {
   if (!video) {
-    return <div></div>;
+    return <div>Loading...</div>;
   }
 
   const videoSource = `https://www.youtube.com/embed/${video.id.videoId}`
@@ -10,7 +10,7 @@ const VideoDetail = ({ video }) => {
   return (
     <div>
       <div className="ui embed">
-        <iframe src={videoSource} />
+        <iframe title="video player" src={videoSource} />
       </div>
       <div className="ui segment">
         <h4 className="ui header">{video.snippet.title}</h4>
