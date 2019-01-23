5f3cb5bac9c8dfd090efa71aac9d9b3920be2bd5
melissasedgwick <melissa.sedgwick@yahoo.co.uk>
2019-01-23 15:27:01 +0000

    Selected video is displayed

--- videos/src/components/VideoDetail.js
+++ videos/src/components/VideoDetail.js
@@ -5,8 +5,13 @@ const VideoDetail = ({ video }) => {
     return <div></div>;
   }
 
+  const videoSource = `https://www.youtube.com/embed/${video.id.videoId}`
+
   return (
     <div>
+      <div className="ui embed">
+        <iframe src={videoSource} />
+      </div>
       <div className="ui segment">
         <h4 className="ui header">{video.snippet.title}</h4>
         <p>{video.snippet.description}</p>
