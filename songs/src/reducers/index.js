954a8ec995598111d2f5af870282cbb365fcb24c
melissasedgwick <melissa.sedgwick@yahoo.co.uk>
2019-01-25 15:04:28 +0000

    Set up songs project with Redux - displays hard-coded list of songs

---
+++ songs/src/reducers/index.js
@@ -0,0 +1,23 @@
+import { combineReducers } from 'redux';
+
+const songsReducer = () => {
+  return [
+    { title: 'No Scrubs', duration: '4:05' },
+    { title: 'All Star', duration: '3:15' },
+    { title: 'Everlong', duration: '3:55' },
+    { title: 'Bad Company', duration: '5:05' }
+  ];
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+  if (action.type === 'SONG_SELECTED') {
+    return action.payload;
+  }
+
+  return selectedSong;
+};
+
+export default combineReducers({
+  songs: songsReducer,
+  selectedSong: selectedSongReducer
+});
