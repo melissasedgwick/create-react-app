a10c00a7c877251cd28dedcc552ac9bfe65bfb5e
melissasedgwick <melissa.sedgwick@yahoo.co.uk>
2019-02-12 16:00:06 +0000

    Added basic components with routing

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -1,31 +1,21 @@
 import React from 'react';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
-
-const PageOne = () => {
-  return (
-    <div>
-      PageOne
-      <Link to="/pagetwo">Page Two</Link>
-    </div>
-  );
-};
-
-const PageTwo = () => {
-  return (
-    <div>
-      PageTwo
-      <Link to="/">Page One</Link>
-    </div>
-  );
-};
+import StreamCreate from './streams/StreamCreate.js'
+import StreamDelete from './streams/StreamDelete.js'
+import StreamEdit from './streams/StreamEdit.js'
+import StreamList from './streams/StreamList.js'
+import StreamShow from './streams/StreamShow.js'
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
         <div>
-          <Route path="/" exact component={PageOne} />
-          <Route path="/pagetwo" component={PageTwo} />
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/new" component={StreamCreate} />
+          <Route path="/streams/edit" component={StreamEdit} />
+          <Route path="/streams/delete" component={StreamDelete} />
+          <Route path="/streams/show" component={StreamShow} />
         </div>
       </BrowserRouter>
     </div>
